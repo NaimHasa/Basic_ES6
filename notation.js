41248f6e48c5b14620e319016ea3937d222ca0f0
Naim Hasan <naimarafat7@gmail.com>
2023-06-04 04:52:13 -0700

    notation

---
+++ notation.js
@@ -0,0 +1,14 @@
+const student =
+{
+    name: 'Naim Hasan',
+    Roll: 150913,
+    Dep: 'Computer',
+    marks: {
+        math: 15,
+        cremasty: 20,
+        physice: 30
+    }
+
+};
+const studetnMarks = student.marks.math;
+console.log(studetnMarks);
